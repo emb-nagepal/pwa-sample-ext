fa3e7e0280be08557b34aaba949a94c0258f3a10
emb-nagepal <97872607+emb-nagepal@users.noreply.github.com>
2022-02-22 16:54:33 +0530

    "Taglist Extension as per docs in pwa studio doc Type-1"

--- packages/@excart/sample/src/componentOverrideMapping.js
+++ packages/@excart/sample/src/componentOverrideMapping.js
@@ -7,4 +7,5 @@ module.exports = componentOverride = {
     [`@magento/venia-ui/lib/components/App/index.js`]: './packages/@excart/sample/src/overrides/App/index.js',
     [`@magento/venia-ui/lib/components/Header/header.js`]: '@excart/sample/src/overrides/Header/header.js',
     [`@magento/venia-ui/lib/components/Footer/footer.js`]: './packages/@excart/sample/src/overrides/Footer/footer.js',
+    [`@magento/venia-ui/lib/components/ProductFullDetail/productFullDetail.js`]: '@excart/sample/src/overrides/ProductFullDetail/productFullDetail.js',
 };
